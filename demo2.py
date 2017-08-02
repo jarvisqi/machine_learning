3e230c3582a313eea8b12892ddf0866c8daee46c
jarvisqi <qdw8911@sina.com>
2017-08-02 16:56:38 +0800

    update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,4 +1,5 @@
 import pynlpir
+from ctypes import c_char_p
 from pynlpir import nlpir
 
 import warnings
@@ -19,16 +20,27 @@ def word_Segment():
     in_text = codecs.open('data/xuezhong.txt', 'r', encoding='UTF-8').read()
     pynlpir.open()
     # 文件分词
+    nlpir.AddUserWord(c_char_p("徐骁".encode()))
+    nlpir.AddUserWord(c_char_p("徐渭熊".encode()))
+    nlpir.AddUserWord(c_char_p("徐北枳".encode()))
+    nlpir.AddUserWord(c_char_p("白狐儿脸".encode()))
+    nlpir.AddUserWord(c_char_p("轩辕青锋".encode()))
+    nlpir.AddUserWord(c_char_p("姜泥".encode()))
+    nlpir.AddUserWord(c_char_p("大官子".encode()))
+    nlpir.AddUserWord(c_char_p("北凉".encode()))
+    nlpir.AddUserWord(c_char_p("小和尚".encode()))
+
     nlpir.FileProcess('data/xuezhong.txt'.encode("utf-8"),
                       'data/xuezhong_seg_1.txt'.encode("utf-8"), False)
     # key_words = pynlpir.get_key_words(in_text, max_words=20, weighted=True)
     # print(key_words)
 
 
-def main():
+def mode_traing():
+
     sentences = word2vec.Text8Corpus('data/xuezhong_seg_1.txt')
     model = word2vec.Word2Vec(
-        sentences, min_count=20, size=800, window=8, workers=3)
+        sentences, min_count=20, size=2000, window=10, workers=3)
     # 计算两个词的相似度/相关程度
     simil_1 = model.similarity(u"王仙芝", u"怪物")
     simil_2 = model.similarity(u"徐凤年", u"殿下")
@@ -42,11 +54,21 @@ def main():
     # 保存模型，以便重用
     model.save(u"models/xue.model")
 
-    # 加载模型
-    # model_2 = word2vec.Word2Vec.load("text8.model")
-
     print('finished')
 
 
+def main():
+    # 分词
+    word_Segment()
+    # 模型训练并保存
+    mode_traing()
+
+    # 模型使用
+    w_model = word2vec.Word2Vec.load(u"models/xue.model")
+    siml = w_model.similarity(u"褚禄山", u"胖子")
+    print("【褚禄山】和【胖子】相似度：", siml)
+
+
 if __name__ == '__main__':
+
     main()
